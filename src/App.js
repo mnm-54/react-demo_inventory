6d5557b1b0de2dc4fb952474db22665a72841b1e
mnm-54 <shahriarmun54@gmail.com>
2022-07-05 01:03:01 +0600

    feat(App.js): get and delete req

--- src/App.js
+++ src/App.js
@@ -2,24 +2,35 @@ import "./App.css";
 import React, { Component } from "react";
 import NavBar from "./components/navbar";
 import Products from "./components/products";
-import ProductInput from "./components/productInput";
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: `http://127.0.0.1:3000/`,
+});
 
 class App extends Component {
   state = {
-    products: [
-      { id: 1, name: "laptop", brand: "hp", amount: 100, price: 10000 },
-      { id: 2, name: "monitor", brand: "dell", amount: 250, price: 100 },
-      { id: 3, name: "keyboard", brand: "unbranded", amount: 500, price: 50 },
-      { id: 4, name: "notepad", brand: "lenovo", amount: 150, price: 150 },
-    ],
+    products: [],
   };
+
+  constructor() {
+    super();
+    api.get("/").then((res) => {
+      this.setState({ products: res.data });
+    });
+  }
 
   handleDelete = (productId) => {
     const products = this.state.products.filter(
       (product) => product.id !== productId
     );
     this.setState({ products: products });
+    this.deleteProduct(productId);
   };
+  deleteProduct = async (id) => {
+    let data = await api.delete(`/products/${id}`);
+  };
+
   handleIncrement = (product) => {
     const products = [...this.state.products];
     const index = products.indexOf(product);
