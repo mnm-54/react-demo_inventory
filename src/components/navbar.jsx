1a0c2d47e2ab17feca97c19da20683446e33b5f9
mnm-54 <shahriarmun54@gmail.com>
2022-07-06 14:05:12 +0600

    refactor(navbar.jsx): to functional component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,28 +1,23 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class NavBar extends Component {
-  state = {};
-  render() {
-    return (
-      <nav className="navbar bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Inventory{" "}
-            <span className="badge bg-pill bg-secondary">
-              {this.productCount()}
-            </span>
-          </a>
-        </div>
-      </nav>
-    );
-  }
-
-  productCount = () => {
-    let sum = this.props.amountProducts.reduce(function (x, y) {
+function NavBar(props) {
+  const productCount = () => {
+    let sum = props.amountProducts.reduce(function (x, y) {
       return x + y;
     }, 0);
     return sum;
   };
+
+  return (
+    <nav className="navbar bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Inventory{" "}
+          <span className="badge bg-pill bg-secondary">{productCount()}</span>
+        </a>
+      </div>
+    </nav>
+  );
 }
 
 export default NavBar;
