7c3fb1c5440d29595638babd006f7314f8a5967d
mnm-54 <shahriarmun54@gmail.com>
2022-07-06 13:57:28 +0600

    refactor(products.jsx):to functional component

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,42 +1,41 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Product from "./product";
 
-class Products extends Component {
-  styles = {
+function Products(props) {
+  let styles = {
     width: "80%",
   };
-  render() {
-    return (
-      <div>
-        <button
-          className="btn btn-danger btn-sm m-2"
-          onClick={() => this.props.onReset()}
-        >
-          Reset
-        </button>
-        <table className="table table-striped m-2" style={this.styles}>
-          <thead>
-            <tr>
-              <th>Product name</th>
-              <th>Brand</th>
-              <th>Amount</th>
-              <th>Price</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.props.products.map((product) => (
-              <Product
-                key={product.id}
-                onDelete={this.props.onDelete}
-                product={product}
-                onIncrement={this.props.onIncrement}
-              />
-            ))}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
+
+  return (
+    <div>
+      <button
+        className="btn btn-danger btn-sm m-2"
+        onClick={() => props.onReset()}
+      >
+        Reset
+      </button>
+      <table className="table table-striped m-2" style={styles}>
+        <thead>
+          <tr>
+            <th>Product name</th>
+            <th>Brand</th>
+            <th>Amount</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.products.map((product) => (
+            <Product
+              key={product.id}
+              onDelete={props.onDelete}
+              product={product}
+              onIncrement={props.onIncrement}
+            />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default Products;
