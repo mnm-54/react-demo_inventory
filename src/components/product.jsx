79d6789156aa3a52668f9f89679f2a24f747de7d
mnm-54 <shahriarmun54@gmail.com>
2022-07-06 13:52:34 +0600

    refactor(product.jsx):to functional component

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -1,34 +1,32 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class Product extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <tr>
-          <td>{this.props.product.name} </td>
-          <td>{this.props.product.brand} </td>
-          <td>{this.props.product.amount}</td>
-          <td>{this.props.product.price} </td>
-          <td>
-            <button
-              className="btn btn-dark btn-sm m-2"
-              onClick={() => {
-                this.props.onIncrement(this.props.product);
-              }}
-            >
-              Increment
-            </button>
-            <button
-              className="btn btn-danger btn-sm m-2"
-              onClick={() => this.props.onDelete(this.props.product.id)}
-            >
-              Delete
-            </button>
-          </td>
-        </tr>
-      </React.Fragment>
-    );
-  }
+function Product(props) {
+  return (
+    <React.Fragment>
+      <tr>
+        <td>{props.product.name} </td>
+        <td>{props.product.brand} </td>
+        <td>{props.product.amount}</td>
+        <td>{props.product.price} </td>
+        <td>
+          <button
+            className="btn btn-dark btn-sm m-2"
+            onClick={() => {
+              props.onIncrement(props.product);
+            }}
+          >
+            Increment
+          </button>
+          <button
+            className="btn btn-danger btn-sm m-2"
+            onClick={() => props.onDelete(props.product.id)}
+          >
+            Delete
+          </button>
+        </td>
+      </tr>
+    </React.Fragment>
+  );
 }
 
 export default Product;
